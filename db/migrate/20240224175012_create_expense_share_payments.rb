19778180a65ab19bce6508049fe774dc9318b2a0
Daniel Piña <danielalexpg@gmail.com>
2024-02-24 13:25:19 -0600

    [DP-BAJ1] - Fix share payment table name

--- db/migrate/20240224175012_create_expense_share_payments.rb
+++ db/migrate/20240224175012_create_expense_share_payments.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-class CreatePayments < ActiveRecord::Migration[6.1]
+class CreateExpenseSharePayments < ActiveRecord::Migration[6.1]
   def change
-    create_table :payments do |t|
+    create_table :expense_share_payments do |t|
       t.references :expense_share, null: false, foreign_key: true, index: true
       t.references :payment_method, null: false, foreign_key: true, index: true
       t.decimal :amount, precision: 10, scale: 2, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,16 @@ ActiveRecord::Schema.define(version: 2024_02_24_175358) do
     t.index ["user_id"], name: "index_expense_categories_on_user_id"
   end
 
+  create_table "expense_share_payments", force: :cascade do |t|
+    t.bigint "expense_share_id", null: false
+    t.bigint "payment_method_id", null: false
+    t.decimal "amount", precision: 10, scale: 2, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["expense_share_id"], name: "index_expense_share_payments_on_expense_share_id"
+    t.index ["payment_method_id"], name: "index_expense_share_payments_on_payment_method_id"
+  end
+
   create_table "expense_shares", force: :cascade do |t|
     t.bigint "expense_id", null: false
     t.bigint "user_id", null: false
@@ -88,16 +98,6 @@ ActiveRecord::Schema.define(version: 2024_02_24_175358) do
     t.index ["user_id"], name: "index_payment_methods_on_user_id"
   end
 
-  create_table "payments", force: :cascade do |t|
-    t.bigint "expense_share_id", null: false
-    t.bigint "payment_method_id", null: false
-    t.decimal "amount", precision: 10, scale: 2, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["expense_share_id"], name: "index_payments_on_expense_share_id"
-    t.index ["payment_method_id"], name: "index_payments_on_payment_method_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -129,11 +129,11 @@ ActiveRecord::Schema.define(version: 2024_02_24_175358) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "expense_categories", "users"
+  add_foreign_key "expense_share_payments", "expense_shares"
+  add_foreign_key "expense_share_payments", "payment_methods"
   add_foreign_key "expense_shares", "expenses"
   add_foreign_key "expense_shares", "users"
   add_foreign_key "expenses", "expense_categories", column: "category_id"
   add_foreign_key "expenses", "users", column: "owner_id"
   add_foreign_key "payment_methods", "users"
-  add_foreign_key "payments", "expense_shares"
-  add_foreign_key "payments", "payment_methods"
 end
