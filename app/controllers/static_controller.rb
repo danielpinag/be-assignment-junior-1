59e10f11163e42ab5828eb8f93c6074db7ed8245
Daniel Piña <danielalexpg@gmail.com>
2024-02-27 02:48:54 -0600

    [DP-BAJ1] - Add friends section

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,7 @@
 class StaticController < ApplicationController
   def dashboard
+    # TODO: Add cookies to get user's most-interacted-with friends
+    @friends = current_user.friends.first(5)
   end
 
   def person
